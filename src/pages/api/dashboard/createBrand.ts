f4a0a304e15ad4b575e2a9be57994ffcf2e6298e
Fábio Madeira <fabio_madeira1987@outlook.com>
2021-12-27 18:13:58 +0000

    test deploy

---
+++ src/pages/api/dashboard/createBrand.ts
@@ -0,0 +1,18 @@
+import prisma from "../../../lib/prisma/prisma";
+
+// POST /api/post
+// Required fields in body: title
+// Optional fields in body: content
+export default async function handle(req, res) {
+  const { title, content } = req.body;
+
+//   const session = await getSession({ req });
+//   const result = await prisma.car.create({
+//     data: {
+//       title: title,
+//       content: content,
+//       author: { connect: { email: session?.user?.email } },
+//     },
+//   });
+  res.json(null);
+}
--- src/pages/api/dashboard/createCar.ts
+++ src/pages/api/dashboard/createCar.ts
@@ -1,4 +1,4 @@
-import { getSession } from "next-auth/client";
+
 import prisma from "../../../lib/prisma/prisma";
 
 // POST /api/post
@@ -7,13 +7,13 @@ import prisma from "../../../lib/prisma/prisma";
 export default async function handle(req, res) {
   const { title, content } = req.body;
 
-  const session = await getSession({ req });
-  const result = await prisma.car.create({
-    data: {
-      title: title,
-      content: content,
-      author: { connect: { email: session?.user?.email } },
-    },
-  });
-  res.json(result);
+  // const session = await getSession({ req });
+  // const result = await prisma.car.create({
+  //   data: {
+  //     title: title,
+  //     content: content,
+  //     author: { connect: { email: session?.user?.email } },
+  //   },
+  // });
+  res.json(null);
 }
